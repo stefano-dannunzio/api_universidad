09fbccd6545008c099a789f3d1f0743c2c200aa5
Stefano D'Annunzio <89990955+stefano-dannunzio@users.noreply.github.com>
2024-02-16 16:28:18 -0300

    MateriaController.java terminado.

---
+++ src/main/java/com/stefanodannunzio/api_universidad/controller/MateriaController.java
@@ -0,0 +1,51 @@
+package com.stefanodannunzio.api_universidad.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import com.stefanodannunzio.api_universidad.business.MateriaService;
+import com.stefanodannunzio.api_universidad.model.Materia;
+import com.stefanodannunzio.api_universidad.model.dto.MateriaDto;
+import com.stefanodannunzio.api_universidad.persistence.exception.MateriaNotFoundException;
+
+
+@RestController
+@RequestMapping("materia")
+public class MateriaController {
+
+    @Autowired
+    private MateriaService materiaService;
+
+    //Crear materia
+    @PostMapping
+    public void crearMateria(@RequestBody MateriaDto materiaDto) {
+        materiaService.crearMateria(materiaDto);
+    }
+
+    //Modificar materia
+    @PutMapping("/{idMateria}")
+    public void modificarMateria(@PathVariable int idMateria, @RequestBody MateriaDto materiaDto) throws IllegalArgumentException, MateriaNotFoundException {
+        materiaService.modificarMateria(idMateria, materiaDto);
+    }
+
+    //Eliminar materia
+    @DeleteMapping("/{idMateria}")
+    public void eliminarMateria(@PathVariable int idMateria) throws MateriaNotFoundException {
+        materiaService.eliminarMateria(idMateria);
+    }
+
+    //Obtener materia por nombre
+    @GetMapping()
+    public Materia obtenerMateriaPorNombre(@RequestParam String nombre) throws MateriaNotFoundException {
+        return materiaService.obtenerMateriaPorNombre(nombre);
+    }
+
+    //Listar todas las materias ordenadas por nombre ascendente o descendente y código ascendente o descendente
+    @GetMapping()
+    public List<Materia> listarMaterias(@RequestParam String order) throws MateriaNotFoundException {
+        return materiaService.listarMaterias(order);
+    }
+    
+}
